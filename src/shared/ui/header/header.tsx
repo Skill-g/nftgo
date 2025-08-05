7a2fdd5920fc49a24b15dd6113315891154dbfa5
Skill-g <nikitaepifantsevv@gmail.com>
2025-08-05 11:22:24 +0500

    Update: update Disconnect button

--- src/shared/ui/header/header.tsx
+++ src/shared/ui/header/header.tsx
@@ -73,7 +73,7 @@ export function Header (){
                             </span>
                             <Button
                                 onClick={handleConnect}
-                                className="bg-red-600 hover:bg-red-700 text-white px-4 py-2 rounded-lg text-sm">
+                                className="bg-red-600 hover:bg-red-700 text-white px-2 rounded-lg text-sm">
                                 Disconnect
                             </Button>
                         </div>
